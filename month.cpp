e71dc360ba14c0cfb7fb59b142d07aa000fa7d70
System Administrator <root@Zhipeng.home>
2021-09-02 09:50:14 -0400

    task D

--- month.cpp
+++ month.cpp
@@ -35,7 +35,7 @@ int main() {
 	}
 	
 	int daysInCurrentMonth = 0;
-	if (month == 1 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
+	if (month == 1 || month == 3 || month == 5 || month == 7 || month == 8 || month == 10 || month == 12){
 		daysInCurrentMonth = 31;
 	}
 	else if (month == 4 || month == 6 || month == 9 || month == 11){
